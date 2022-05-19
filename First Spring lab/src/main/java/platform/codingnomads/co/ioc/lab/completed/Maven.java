34be366d355b40194a3f31dc34d468e0ce50141f
samve <sam.verner42@gmail.com>
2022-05-18 23:27:16 -0500

    IoC Lab

---
+++ First Spring lab/src/main/java/platform/codingnomads/co/ioc/lab/completed/Maven.java
@@ -0,0 +1,14 @@
+package platform.codingnomads.co.ioc.lab.completed;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Maven {
+    private String version;
+}
