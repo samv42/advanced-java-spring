b6c4798b2314208863d8179d83bfcd88f2f47695
samve <sam.verner42@gmail.com>
2022-07-07 14:04:42 -0500

    spring web

---
+++ First Spring lab/src/main/java/platform/codingnomads/co/springweb/resttemplate/GET/models/Excuse.java
@@ -0,0 +1,10 @@
+package platform.codingnomads.co.springweb.resttemplate.GET.models;
+
+import lombok.Data;
+
+@Data
+public class Excuse {
+    long id;
+    String excuse;
+    String category;
+}
---
+++ First Spring lab/src/main/java/platform/codingnomads/co/springweb/resttemplate/GET/models/ExcuseTemplate.java
@@ -0,0 +1,13 @@
+package platform.codingnomads.co.springweb.resttemplate.GET.models;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class ExcuseTemplate {
+    //Excuse[] excuses;
+    long id;
+    String excuse;
+     String category;
+}
